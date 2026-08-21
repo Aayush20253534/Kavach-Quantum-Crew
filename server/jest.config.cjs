4c369e5c09a6d9027679a7957476902b8d7de3a3
Aayush20253534 <aayush.20253534@mnnit.ac.in>
2026-08-21 22:19:16 +0530

    feat: implemented disaster management and responder backend

--- server/jest.config.cjs
+++ server/jest.config.cjs
@@ -12,6 +12,7 @@ module.exports = {
     "<rootDir>/tests/phase8/**/*.test.js",
     "<rootDir>/tests/phase9/**/*.test.js",
     "<rootDir>/tests/phase10/**/*.test.js",
+    "<rootDir>/tests/phase11/**/*.test.js",
   ],
   clearMocks: true,
   restoreMocks: true,
